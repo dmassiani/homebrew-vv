16192a1966a33ffc8d537e0fff738bded755e305
dmassiani <davidmassianirennes@gmail.com>
2016-08-04 16:17:10 +0200

    update

--- vv.rb
+++ vv.rb
@@ -1,6 +1,6 @@
 class Vv < Formula
-  homepage "https://github.com/bradp/vv"
-  url "https://github.com/bradp/vv/archive/1.11.tar.gz"
+  homepage "https://github.com/dmassiani/vv"
+  url "https://github.com/dmassiani/vv/archive/master.zip"
   sha256 "0a4e160edad27eecaecf5cb4798f8067031214403cecc5635cbba90bc8a8cfd0"
 
   def install
